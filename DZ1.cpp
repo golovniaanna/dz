8183d17569d4063743bbffb846368776129ddb23
anna <72653455+golovniaanna@users.noreply.github.com>
2020-10-10 14:39:28 +0300

    Add files via upload

---
+++ DZ1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int a = 0, b = 0;
+    a = 15;
+    b = 20;
+    std::cout << a << " + " << b << " = " << a + b << endl;
+    return 0;
+}
---
+++ dz2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 0, b = 0, c = 0, d = 0, e = 0, f = 3;
+	a = 13;
+	b = 33;
+	c = 4;
+	d = 4;
+	e = 2;
+	f = 3;
+	cout << a << " * " << e << " + " << b << " / " << f << " + " << c << " * " << d << " = " << a + b + c * d << endl;
+	return 0;
+}

5eae543fe53d07278cb40b10527fede76dc17a20
anna <72653455+golovniaanna@users.noreply.github.com>
2020-11-21 14:49:50 +0300

    Add files via upload

--- ConsoleApplication9.cpp
+++ ConsoleApplication9.cpp
@@ -1,19 +1,19 @@
 ﻿#include <iostream>
 
-void printArr(int* arr, int lenArr = 0);
+void printArr(int arr[], int lenArr = 0);
 
 int main() {
     int arr[2] = { 0, 1 };
     printf("%d", sizeof(arr));
     printf("%s ", "Enter the amount of either 0 1 you want to be printed : ");
-    printArr(arr);
+    printArr( arr, sizeof(arr)/4);
 
 
     return 0;
 }
 
 void printArr(int arr[], int lenArr) {
-    for (int i = 0; i < sizeof(arr) / 4; i++) {
+    for (int i = 0; i < lenArr; i++) {
         printf("%d ", arr[i]);
     }
  }
